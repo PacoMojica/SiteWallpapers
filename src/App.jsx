c5bf00c727a9b4339019e448d94c93df9e9c6894
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-22 22:14:02 -0600

    añado la lista en el localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Lista_wallpapers } from "./components/Cards/ListaWallpapers";
 import Home from "./components/Home";
 import Error404 from "./components/404";
 import Form from "./components/Form/Form";
 import Contacto from "./components/Contacto";
 import Anime from "./Site/Anime";
+import { Lista_wallpapers } from "./components/Cards/ListaWallpapers";
 
 function App() {
   const [lista, actualizarLista] = useState(Lista_wallpapers);
 
-  const containerWallpaper = (Data) => {
+  function containerWallpaper(Data) {
     actualizarLista(...lista, Data);
-  };
+  }
 
   return (
     <>
