48ab430b9e266e20e5cf0e31e9b910f72b0e8dcd
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-29 22:15:03 -0600

    quito animación de la img

--- src/components/Cards/MapCards.jsx
+++ src/components/Cards/MapCards.jsx
@@ -16,19 +16,20 @@ function MapCards({ itemWallpapers }) {
   }
 
   // Animación de img
-  const [AnimaciónImg, setAnimaciónImg] = useState(false);
+  // const [AnimaciónImg, setAnimaciónImg] = useState(false);
 
-  function handleAnimación() {
-    setAnimaciónImg(!AnimaciónImg);
-  }
+  // function handleAnimación() {
+  //   setAnimaciónImg(!AnimaciónImg);
+  // }
 
   return (
     <>
       <div className="card d-inline-flex flex-wrap justify-content-center align-content-center">
         <img
           src={UrlWallpaper}
-          className={`box ${AnimaciónImg ? "scaled" : ""}`}
-          onMouseLeave={handleAnimación}
+          style={{ width: "100%", height: "200px" }}
+          // className={`box ${AnimaciónImg ? "scaled" : ""}`}
+          // onMouseLeave={handleAnimación}
           alt={NombreWallpaper}
         />
         <div className="card-body">
