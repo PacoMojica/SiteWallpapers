006e6c1bb146ac2742422ad723bcb087779d1136
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-09-17 11:45:57 -0600

    añado animación a img en el home

--- src/components/Cards/MapCards.jsx
+++ src/components/Cards/MapCards.jsx
@@ -8,45 +8,16 @@ function MapCards({ itemWallpapers }) {
     NombreWallpaper,
     NombreDeLaPersona,
   } = itemWallpapers;
-  // Estado del like
-  // const [Like, setLike] = useState(0);
-
-  // function OnLike() {
-  //   setLike(Like + 1);
-  // }
-
-  // Animación de img
-  // const [AnimaciónImg, setAnimaciónImg] = useState(false);
-
-  // function handleAnimación() {
-  //   setAnimaciónImg(!AnimaciónImg);
-  // }
 
   return (
     <>
-      <div className="card">
+      <a href={DownloadWallpaper} target="_Black">
         <img
           className="card-img-top card-image"
           src={UrlWallpaper}
-          // className={`box ${AnimaciónImg ? "scaled" : ""}`}
-          // onMouseLeave={handleAnimación}
           alt={NombreWallpaper}
         />
-
-        <div className="card-body">
-          <h5 className="card-title fs-5">{NombreWallpaper}</h5>
-          <p>Persona que agrego el wallpaper: {NombreDeLaPersona}</p>
-          <a
-            href={DownloadWallpaper}
-            className="btn btn-primary"
-            target="_Black"
-          >
-            Download
-          </a>
-          {/* <i className="bi bi-heart m-4 fs-4" onClick={OnLike} /> */}
-          {/* <p>Likes {Like}</p> */}
-        </div>
-      </div>
+      </a>
     </>
   );
 }
